8ec6058de279c3a5ceb03ca01df371e831241f97
Gouyichen <3038211493@qq.com>
2020-06-15 19:52:37 +0800

    提交

--- blog/routes/indexRouter.js
+++ blog/routes/indexRouter.js
@@ -1,7 +1,7 @@
 //1.导入express
 const express = require('express');
 //2.使用 express.Router创建路由
-const router = express.Router();
+const router = express.Router();                                                                                                                                   
 //引入控制器代码
 let { show, login, logout } = require('../controller/indexCtrl');
 // 3. 创建路由
